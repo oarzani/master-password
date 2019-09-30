00e850dadbebf34e3a44654f9bf755f60b89c5c5
Omid Arzani <omid.arzani@gmx.net>
2019-09-30 17:04:26 +0200

    query and commands added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
-const { writeSecrets } = require("./models/secret");
-const { readSecrets } = require("./models/secret");
+const { executeCommand } = require("./lib/commands");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 /* commands 
 set {key} {value}
@@ -34,68 +39,86 @@ get {key}
 const userArgv = process.argv.slice(2);
 const [action, key, value] = userArgv;
 
-console.log(action, key, value);
+// console.log(action, key, value);
 
-function set(key, value) {
-  const secrets = readSecrets();
-  secrets[key] = value;
+// function set(key, value) {
+//   const secrets = readSecrets();
+//   secrets[key] = value;
 
-  writeSecrets(secrets);
-}
-
-function unset(key) {
-  const secrets = readSecrets();
-  delete secrets[key];
-  writeSecrets(secrets);
-}
-
-function get(key) {
-  const secrets = readSecrets();
-
-  const secret = secrets[key];
-  console.log(secret);
-}
-
-// set(key, value);
-//call the correct funktion base on action
-// //Soluzion 1
-// switch (action) {
-//   case "get":
-//     get(key);
-//     break;
-//   case "set":
-//     set(key, value);
-//     break;
-//   case "unset":
-//     unset(key);
-//     break;
-//   default:
-//     throw new Error("unknown action");
+//   writeSecrets(secrets);
 // }
 
-// solution 2:
-// function perform() {
-//   if (action === "set") {
-//     set(key, value);
-//   } else if (action === "unset") {
-//     unset(key);
-//   } else if (action === "get") {
-//     get(key);
-//   } else {
+// function unset(key) {
+//   const secrets = readSecrets();
+//   delete secrets[key];
+//   writeSecrets(secrets);
+// }
+
+// function get(key) {
+//   const secrets = readSecrets();
+
+//   const secret = secrets[key];
+//   console.log(secret);
+// }
+
+// // set(key, value);
+// //call the correct funktion base on action
+// // //Soluzion 1
+// // switch (action) {
+// //   case "get":
+// //     get(key);
+// //     break;
+// //   case "set":
+// //     set(key, value);
+// //     break;
+// //   case "unset":
+// //     unset(key);
+// //     break;
+// //   default:
+// //     throw new Error("unknown action");
+// // }
+
+// // solution 2:
+// // function perform() {
+// //   if (action === "set") {
+// //     set(key, value);
+// //   } else if (action === "unset") {
+// //     unset(key);
+// //   } else if (action === "get") {
+// //     get(key);
+// //   } else {
+// //     throw new Error("unknown action");
+// //   }
+// // }
+// // perform();
+
+// // solution 3:
+
+// const commands = {
+//   set: set,
+//   get: get,
+//   unset: unset
+// };
+// function executeCommand(action) {
+//   const command = commands[action];
+//   if (!command) {
 //     throw new Error("unknown action");
 //   }
+//   command(key, value);
 // }
-// perform();
 
-// solution 3:
-const commands = {
-  set: set,
-  get: get,
-  unset: unset
+const masterPassword = "abc";
+rl.question("What is the master-password? ", password => {
+  if (password === masterPassword) {
+    executeCommand(action, key, value);
+  } else {
+    console.log("invalid password");
+  }
+
+  rl.close();
+});
+
+// Override default output to hide password
+rl._writeToOutput = function _writeToOutput() {
+  rl.output.write("*");
 };
-
-const command = commands[action];
-if (!command) {
-  throw new Error("unknown action");
-}
-command(key, value);
