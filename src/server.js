480f0224abd6ad49982e182887bbef1b396c2f66
Omid Arzani <omid.arzani@gmx.net>
2019-10-02 11:04:26 +0200

    add parse and createElement

--- src/server.js
+++ src/server.js
@@ -9,20 +9,25 @@ const http = require("http");
 // });
 
 //Beispiel oben ruft nur die JSON auf
-
+const fs = require("fs");
 const { get } = require("./lib/commands");
+const url = require("url");
 
 const server = http.createServer(function(request, response) {
-  if (request.url === "/favicon.ico") {
+  const { pathname } = url.parse(request.url);
+  // use url.parse to seperate url to pathname and search
+  if (pathname === "/favicon.ico") {
     return response.end();
   }
-  if (request.url === "/") {
-    return response.end("Welcome to my secrets manager");
+  if (pathname === "/") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    const content = fs.readFileSync("src/view/index.html", "utf-8");
+    return response.end(content);
   }
-  console.log("Requested", request.url);
+  console.log("Requested", pathname);
 
   try {
-    const key = request.url;
+    const key = pathname;
     const res = key.slice(1);
 
     const secret = get("leon", res);
@@ -34,4 +39,6 @@ const server = http.createServer(function(request, response) {
   response.end();
 });
 
-server.listen(7000);
+server.listen(7000, () => {
+  console.log("Server listens n http://localhost:7000");
+});
